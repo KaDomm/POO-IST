3f616b6b361ce09909e0d04a37f753b4837c111e
Nuno Ramos <nuno.ramos@ctw.bmwgroup.com>
2020-04-18 10:01:29 +0100

    1

--- Classification/src/aplication/Program.java
+++ Classification/src/aplication/Program.java
@@ -7,40 +7,28 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		final String file1 = args[0];
-		final String file2 = args.length > 1 ? args[1] : null ;
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(file1))){
-			
-			System.out.println("file 1 ------------------------\n\n");
-			String line = br.readLine();
-			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-	
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(file2))){
-			
-			System.out.println("file 2 ------------------------\n\n");
-			
-			String line = br.readLine();
-			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-	
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+
+		if (args.length == 0) {
+			// This program must have at least one command-line argument to work with.
+			System.out.println("Usage:   java LineCounts <file-names>");
+			return;
 		}
 
+		for (String arg : args) {
+			try (BufferedReader br = new BufferedReader(new FileReader(arg))) {
+
+				System.out.println("file 1 ------------------------\n\n");
+				String line = br.readLine();
+
+				while (line != null) {
+					System.out.println(line);
+					line = br.readLine();
+				}
+
+			} catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+
+		}
 	}
-
 }

bb20f7511905969ace6be1de6480c45a46ae41a6
matth <matth@LAPTOP-S5R27MFN.home>
2020-05-07 00:58:19 +0200

    here is my lost work. not over yet

--- Classification/src/entities/Train.java
+++ Classification/src/entities/Train.java
@@ -26,6 +26,84 @@ public class Train {
 	public Train() {
 		super();
 	}
+	//Method to calculate Nijkc taking ijkc as parameters.
+		// to be tested and optimized (get(i) in for loop is bad and brings a large computational time)
+		public Integer Nijkc(int i, int j, int k, int c) {
+			Integer Nijkc = 0;
+			//case with no parents
+			if(i == 0) {
+				int xik = samples.get(i).getArrayOfEntries().get(k);
+				for (int n=0;n<samples.get(i).getR();n++) {
+					if (columnC.getArrayOfEntries().get(n) == c && samples.get(i).getArrayOfEntries().get(n) == xik) {
+						Nijkc++;
+					}
+				}
+			}
+			//case with parents
+			else {
+				int xik = samples.get(i).getArrayOfEntries().get(k);
+				int wij = samples.get(i-1).getArrayOfEntries().get(j);
+				for (int n=0;n<samples.get(i).getR();n++) {
+					if (columnC.getArrayOfEntries().get(n) == c && samples.get(i).getArrayOfEntries().get(n) == xik && samples.get(i-1).getArrayOfEntries().get(n) == wij) {
+						Nijkc++;
+					}
+				}
+			}
+			return Nijkc;
+		}
+		
+		public double alphaLL(int index){
+			//index is i;
+			double alpha = 0;
+			Column ToCalculate = samples.get(index);
+			Column ParentToCalculate = samples.get(index-1);
+			//2D Arraylist containing the valuu of the Sum NJ depending on i,k. the first column is the value c=0; and the second column is value of c=1;
+			ArrayList<ArrayList<Integer>> NJ = new ArrayList<ArrayList<Integer>>();
+			//Nj here is the sum NJ with the kth value;
+			Integer Nj;
+			//2D Arraylist containing the valuu of the Sum NK depending on i,j. the first column is the value c=0; and the second column is value of c=1;
+			ArrayList<ArrayList<Integer>> NK = new ArrayList<ArrayList<Integer>>();
+			//Nk here is the sum NK with the jth value;
+			Integer Nk;
+			//Nc with c = 0 in the first column, c = 1 in the second column
+			ArrayList<Integer> NC = new ArrayList<Integer>();
+			int Nc0 = 0;
+			int Nc1 = 0;
+			int N = ToCalculate.getR();
+			for(int c=0;c<1;c++) {
+				ArrayList<Integer> NJc = new ArrayList<Integer>();
+				ArrayList<Integer> NKc = new ArrayList<Integer>();
+				for(int k=0;k<ToCalculate.getR();k++) {
+					Nj = 0;
+					for(int j=0;j<ParentToCalculate.getR();j++) {
+						Nj = Nj + this.Nijkc(index, j, k, c);
+					}
+					NJc.add(Nj);
+				}
+				for(int j=0;j<ParentToCalculate.getR();j++) {
+					Nk = 0;
+					for(int k=0;k<ToCalculate.getR();k++) {
+						Nk = Nk + this.Nijkc(index, j, k, c);
+					}
+					NKc.add(Nk);
+				}
+				if(columnC.getArrayOfEntries().get(c) == 0) {
+					Nc0++;
+				}
+				else{
+					Nc1++;
+				}
+				NJ.add(NJc);
+				NK.add(NKc);
+			}
+			NC.add(Nc0);
+			NC.add(Nc1);
+			
+			//alpha = ...
+			return alpha;
+		}
+	
+	
 
 	// getters and setter
 	public List<Column> getSamples() {
@@ -72,5 +150,6 @@ public class Train {
 	public String toString() {
 		return "\nsamples: \n" + samples + " \n\n columnC: " + columnC + " \n\n criteria: " + criteria;
 	}
+	
 
 }
